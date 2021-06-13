23714993ddf6892e28caa0f18f111c3635d61bea
Jose Leonard Rico <joselri@macc02f9ch4md6m.americas.manulife.net>
2021-06-13 16:53:53 +0800

    Added mock data and API

--- src/index.test.js
+++ src/index.test.js
@@ -9,12 +9,12 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-  it('should say hello', () => {
+  it('should have h1 with Users text', () => {
     const index = fs.readFileSync("./src/index.html", "utf-8");
     const { JSDOM } = jsdom;
     const dom = new JSDOM(index);
     const h1 = dom.window.document.getElementsByTagName("h1")[0];
-    expect(h1.innerHTML).to.equal("Hello Manulife!");
+    expect(h1.innerHTML).to.equal("Users");
     dom.window.close();
   });
 });
